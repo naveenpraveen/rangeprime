a4533fff1e8b1faea5fdab6bb270e9e48315a71d
naveenpraveen <48952772+naveenpraveen@users.noreply.github.com>
2019-06-18 15:15:15 +0530

    Create arraysmall.c

---
+++ arraysmall.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int a[10],n,b,d,t;
+    scanf("%d\n",&n);
+    for(b=0;b<n;b++)
+    {
+        scanf("%d",&a[b]);
+    }
+    for(b=0;b<n;b++)
+    {
+        for(d=0;d<n;d++)
+        {
+         if(a[d]<a[d])
+         {
+            t=a[b];
+            a[b]=a[d];
+            a[d]=t;
+         }   
+        }
+    }
+    printf("%d",a[0]);
+    return 0;
+}
