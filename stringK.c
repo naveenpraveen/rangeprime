a088a8525e37bcac3d9381ca8eb2e464e5611c64
naveenpraveen <48952772+naveenpraveen@users.noreply.github.com>
2019-07-16 09:29:49 +0530

    Create stringK.c

---
+++ stringK.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    char str[10];
+    scanf("%s",str);
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        printf("%s\n",str);
+    }
+    return 0;
+}
