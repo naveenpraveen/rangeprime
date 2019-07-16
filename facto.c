9fe9570f208676883cb751957688f804bb8bc64c
naveenpraveen <48952772+naveenpraveen@users.noreply.github.com>
2019-07-16 19:50:52 +0530

    Create facto.c

---
+++ facto.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int fact=1,i,n;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        fact=fact*i;
+    }
+    printf("%d",fact);
+    return 0;
+}
