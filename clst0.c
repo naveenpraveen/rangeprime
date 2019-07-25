43a80660d571389f6ba6607d25462580baf10050
naveenpraveen <48952772+naveenpraveen@users.noreply.github.com>
2019-07-25 18:11:46 +0530

    Create clst0.c

---
+++ clst0.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int n,i,j,temp,cnt=0;
+    scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++)
+        scanf("%d",&a[i]);
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(cnt==0 && a[i]+a[j]==0)
+            {
+            printf("%d %d",a[i],a[j]);
+            cnt=cnt+1;
+            }
+        }
+    }
+    return 0;
+}
